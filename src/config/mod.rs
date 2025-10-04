8c92ccf7efadcf96fe68102e09ab2f001350efc5
ElBellaCiao <aguilarvtrading@gmail.com>
2025-10-04 02:15:16 +0000

    inject url through function params

--- src/config/mod.rs
+++
@@ -1,6 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-pub struct Settings {
-    pub telemetry_risk_service_url: String,
-}
--- src/service/mod.rs
+++ src/service/mod.rs
@@ -2,18 +2,13 @@ mod risk_client;
 mod risk_layer;
 
 use crate::client;
-use crate::config::Settings;
 use crate::service::risk_client::RiskClient;
 use crate::service::risk_layer::RiskLayer;
 use anyhow::Result;
-use cloud_util::get_config;
 use std::thread;
 use std::time::Duration;
 
-pub fn get_risk_layer() -> Result<RiskLayer> {
-    // config
-    let config = get_config::<Settings>()?;
-
+pub fn get_risk_layer(telemetry_risk_service_url: String) -> Result<RiskLayer> {
     // helper clients
     let reqwest_client = reqwest::blocking::Client::builder()
         .timeout(Duration::from_secs(5))
@@ -27,12 +22,7 @@ pub fn get_risk_layer() -> Result<RiskLayer> {
 
     // RiskClient
     let (tx, rx) = kanal::unbounded();
-    let risk_client = RiskClient::new(
-        api_client,
-        rx,
-        &config.telemetry_risk_service_url,
-        &instance_id,
-    );
+    let risk_client = RiskClient::new(api_client, rx, &telemetry_risk_service_url, &instance_id);
     let handle = thread::spawn(move || risk_client.run());
 
     // RiskLayer
